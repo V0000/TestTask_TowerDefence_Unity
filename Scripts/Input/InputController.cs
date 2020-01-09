a83156a7c54306c35c4f8d2746aba69f0bc96bbf
v2v2 <00bl@bk.ru>
2020-01-09 23:23:36 +0300

    1

---
+++ Scripts/Input/InputController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+
+namespace Input
+{
+    public class InputController : Singleton<InputController>
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ Scripts/Utilities/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    /// <summary>
+    /// Singleton class
+    /// </summary>
+    /// <typeparam name="T">Type of the singleton</typeparam>
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        /// <summary>
+        /// The static reference to the instance
+        /// </summary>
+        public static T instance { get; protected set; }
+
+        public static bool instanceExists
+        {
+            get { return instance != null; }
+        }
+        /// <summary>
+        /// Associate singleton with instance
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (instanceExists)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = (T) this;
+            }
+        }
+        /// <summary>
+        /// Clear singleton associations
+        /// </summary>
+        protected virtual void OnDestroy()
+        {
+            if (instance == this)
+            {
+                instance = null;
+            }
+        }
+    }
+}
+
