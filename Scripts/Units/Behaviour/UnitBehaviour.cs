d6411abdc5937a008aaebdf3030bd9f8e764cb6a
ryabchenko.vladimir <00bl@bk.ru>
2020-01-28 14:41:15 +0300

    On work

---
+++ Scripts/Units/Behaviour/UnitBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Units
+{
+	public class UnitBehaviour : MonoBehaviour 
+	{
+		[HideInInspector]
+		public bool isEnemy;
+		public Transform fountain;
+		private GameObject target;
+		private NavMeshAgent agent;
+		
+		
+		void Start () 
+		{
+			agent = GetComponent<NavMeshAgent>();
+		}
+		
+
+		void Update () 
+		{
+			UpdateTargets();
+			agent.destination = target.transform.position;
+		}
+
+		void OnDrawGizmos()
+		{
+			Debug.DrawRay(transform.position, target.transform.position);
+		}
+
+
+		private void UpdateTargets ()
+		{
+			if (ObjectRegistry.TargetIsExist(isEnemy))
+			{
+				target = ObjectRegistry.fountain;            
+			}
+			else
+			{
+				target = ObjectRegistry.GetNearestTarget(transform, isEnemy);
+			}
+		}
+	}
+}
